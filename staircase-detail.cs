5287de49a8bf32be6c5ea716f19bf46cf6e06c73
nikojona <51277544+nikojona@users.noreply.github.com>
2023-02-11 22:01:42 +0700

    Create staircase-detail.cs

---
+++ staircase-detail.cs
@@ -0,0 +1,47 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        int strLength = n - 1;
+        for(int i = 1; i <= n; i++)
+        {
+            string space = new String(' ', strLength);
+            string result = new String('#', i);
+            
+            Console.WriteLine(space+result);
+            strLength -= 1;
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
