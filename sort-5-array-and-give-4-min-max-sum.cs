cb74256214ab17a4e4eec9bda0d52eb23cd04a29
nikojona <51277544+nikojona@users.noreply.github.com>
2023-02-11 22:16:40 +0700

    Create sort-5-array-and-give-4-min-max-sum.cs

---
+++ sort-5-array-and-give-4-min-max-sum.cs
@@ -0,0 +1,53 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        arr.Sort();
+        long ans1 = 0;
+        long ans2 = 0;
+        for(int i = 0; i < 4; i++)
+        {
+            ans1 += arr[i];
+        }
+        
+        for(int i = 1; i < 5; i++)
+        {
+            ans2 += arr[i];
+        }
+        
+        Console.WriteLine(ans1+" "+ans2);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
